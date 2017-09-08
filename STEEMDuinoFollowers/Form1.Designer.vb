b74a1d208cf781657e707d265e4bd701f10d6960
Moises Cardona <cardona_76937@students.pupr.edu>
2017-09-08 15:35:54 -0400

    Add project files.

---
+++ STEEMDuinoFollowers/Form1.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 94)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(213, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Display Followers"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(9, 52)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(95, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Arduino COM Port:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(9, 71)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(31, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "COM"
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.Location = New System.Drawing.Point(46, 68)
+        Me.NumericUpDown2.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(76, 20)
+        Me.NumericUpDown2.TabIndex = 9
+        Me.NumericUpDown2.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(27, 9)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(91, 13)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Steem Username:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(30, 25)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(153, 20)
+        Me.TextBox1.TabIndex = 11
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(18, 13)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "@"
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(240, 133)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.NumericUpDown2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "STEEMDuino Followers"
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents NumericUpDown2 As NumericUpDown
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ STEEMDuinoFollowers/Form1.vb
@@ -0,0 +1,50 @@
+﻿Imports System.IO
+Imports System.IO.Ports
+Imports System.Text
+
+Public Class Form1
+    Dim ArduinoSerialPort As New SerialPort
+    Dim COMSetupComplete As Boolean = False
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If COMSetupComplete = False Then
+            Try
+                With ArduinoSerialPort
+                    .PortName = "COM" & NumericUpDown2.Value
+                    .BaudRate = 38400
+                    .Parity = Parity.None
+                    .DataBits = 8
+                    .StopBits = 1
+                    .DtrEnable = True
+                    .ReadTimeout = 30000
+                    .Open()
+                    Threading.Thread.Sleep(1000)
+                End With
+                COMSetupComplete = True
+            Catch ex As Exception
+                COMSetupComplete = False
+            End Try
+        End If
+        If COMSetupComplete = True Then
+            GetFollowersAndSend()
+            Timer1.Interval = 60000
+            Timer1.Start()
+        Else
+            MsgBox("Another program is already using COM" & NumericUpDown2.Value & "." & vbCrLf & vbCrLf &
+                     "Please try again later", MsgBoxStyle.OkOnly + MsgBoxStyle.Information, "COM" & NumericUpDown2.Value & " Not Available")
+        End If
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        GetFollowersAndSend()
+    End Sub
+    Private Sub GetFollowersAndSend()
+        Dim myWebRequest As Net.WebRequest = Net.WebRequest.Create("https://moisescardona.me/steem/getFollowersCount/?a=" & TextBox1.Text)
+        Dim myWebResponse As Net.WebResponse = myWebRequest.GetResponse()
+        Dim ReceiveStream As Stream = myWebResponse.GetResponseStream()
+        Dim encode As Encoding = System.Text.Encoding.GetEncoding("utf-8")
+        Dim readStream As New StreamReader(ReceiveStream, encode)
+        If ArduinoSerialPort.IsOpen = False Then ArduinoSerialPort.Open()
+        ArduinoSerialPort.Write(TextBox1.Text & ":|" & readStream.ReadLine & " followers")
+    End Sub
+End Class
