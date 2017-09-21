cb96c019c1c019da9c60f04c0ca6018dca65f218
Moises Cardona <cardona_76937@students.pupr.edu>
2017-09-21 15:37:05 -0400

    Now saves Username and COM Ports in Settings

--- STEEMDuinoFollowers/Form1.vb
+++ STEEMDuinoFollowers/Form1.vb
@@ -6,33 +6,40 @@ Public Class Form1
     Dim ArduinoSerialPort As New SerialPort
     Dim COMSetupComplete As Boolean = False
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        If COMSetupComplete = False Then
-            Try
-                With ArduinoSerialPort
-                    .PortName = "COM" & NumericUpDown2.Value
-                    .BaudRate = 38400
-                    .Parity = Parity.None
-                    .DataBits = 8
-                    .StopBits = 1
-                    .DtrEnable = True
-                    .ReadTimeout = 30000
-                    .Open()
-                    Threading.Thread.Sleep(1000)
-                End With
-                COMSetupComplete = True
-            Catch ex As Exception
-                COMSetupComplete = False
-            End Try
-        End If
-        If COMSetupComplete = True Then
-            GetFollowersAndSend()
-            Timer1.Interval = 60000
-            Timer1.Start()
-        Else
-            MsgBox("Another program is already using COM" & NumericUpDown2.Value & "." & vbCrLf & vbCrLf &
+        If String.IsNullOrEmpty(TextBox1.Text) = False Then
+            My.Settings.username = TextBox1.Text
+            My.Settings.Save()
+            If COMSetupComplete = False Then
+                Try
+                    With ArduinoSerialPort
+                        .PortName = "COM" & NumericUpDown2.Value
+                        .BaudRate = 38400
+                        .Parity = Parity.None
+                        .DataBits = 8
+                        .StopBits = 1
+                        .DtrEnable = True
+                        .ReadTimeout = 30000
+                        .Open()
+                        Threading.Thread.Sleep(1000)
+                    End With
+                    COMSetupComplete = True
+                Catch ex As Exception
+                    COMSetupComplete = False
+                End Try
+            End If
+            If COMSetupComplete = True Then
+                My.Settings.COMPort = NumericUpDown2.Value
+                My.Settings.Save()
+                GetFollowersAndSend()
+                Timer1.Interval = 60000
+                Timer1.Start()
+            Else
+                MsgBox("Another program is already using COM" & NumericUpDown2.Value & "." & vbCrLf & vbCrLf &
                      "Please try again later", MsgBoxStyle.OkOnly + MsgBoxStyle.Information, "COM" & NumericUpDown2.Value & " Not Available")
+            End If
+        Else
+            MsgBox("You must type your username to send your Follower number to the Arduino")
         End If
-
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
@@ -50,5 +57,14 @@ Public Class Form1
         Catch
             GetFollowersAndSend()
         End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If String.IsNullOrEmpty(My.Settings.username) = False Then
+            TextBox1.Text = My.Settings.username
+        End If
+        If String.IsNullOrEmpty(My.Settings.COMPort) = False Then
+            NumericUpDown2.Value = My.Settings.COMPort
+        End If
     End Sub
 End Class
--- STEEMDuinoFollowers/My Project/Settings.Designer.vb
+++ STEEMDuinoFollowers/My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.1.0.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -52,6 +52,30 @@ Namespace My
 #End If
                 Return defaultInstance
             End Get
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("")>  _
+        Public Property username() As String
+            Get
+                Return CType(Me("username"),String)
+            End Get
+            Set
+                Me("username") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("")>  _
+        Public Property COMPort() As String
+            Get
+                Return CType(Me("COMPort"),String)
+            End Get
+            Set
+                Me("COMPort") = value
+            End Set
         End Property
     End Class
 End Namespace
